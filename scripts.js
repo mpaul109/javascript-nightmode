63a6a8827acc6b75afb40bfa6240fafccc682a0c
mpaul109 <45860227+mpaul109@users.noreply.github.com>
2019-01-03 15:13:22 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("body").toggleClass("main");
+        $("p").toggleClass("main");
+        $("h1").toggleClass("main");
+
+    });
+    $("button").click(function(){
+      $("button").toggleClass("buttondark");
+
+
+    });
+});
